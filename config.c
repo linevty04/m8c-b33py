c01cc4e69283ea2afcc1744bf66e9f852edc4575
RowdyVoyeur <102534097+RowdyVoyeur@users.noreply.github.com>
2023-05-21 17:12:43 +0100

    Disable Cursor

--- config.c
+++ config.c
@@ -25,6 +25,7 @@ config_params_s init_config() {
 
   c.init_fullscreen = 0; // default fullscreen state at load
   c.init_use_gpu = 1;    // default to use hardware acceleration
+  c.init_disable_cursor = 1; // default to disable cursor
   c.idle_ms = 10;        // default to high performance
   c.wait_for_device = 1; // default to exit if device disconnected
   c.wait_packets = 1024; // default zero-byte attempts to disconnect (about 2
@@ -82,7 +83,7 @@ void write_config(config_params_s *conf) {
 
   SDL_Log("Writing config file to %s", config_path);
 
-  const unsigned int INI_LINE_COUNT = 44;
+  const unsigned int INI_LINE_COUNT = 45; // this number needs to be updated if more configurations are added (ini_values)
   const unsigned int LINELEN = 50;
 
   // Entries for the config file
@@ -96,6 +97,8 @@ void write_config(config_params_s *conf) {
   snprintf(ini_values[initPointer++], LINELEN, "idle_ms=%d\n", conf->idle_ms);
   snprintf(ini_values[initPointer++], LINELEN, "wait_for_device=%s\n",
            conf->wait_for_device ? "true" : "false");
+    sprintf(ini_values[initPointer++], "disable_cursor=%s\n",
+            conf->init_disable_cursor ? "true" : "false");
   snprintf(ini_values[initPointer++], LINELEN, "wait_packets=%d\n",
            conf->wait_packets);
   snprintf(ini_values[initPointer++], LINELEN, "[audio]\n");
@@ -242,6 +245,7 @@ void read_audio_config(ini_t *ini, config_params_s *conf) {
 void read_graphics_config(ini_t *ini, config_params_s *conf) {
   const char *param_fs = ini_get(ini, "graphics", "fullscreen");
   const char *param_gpu = ini_get(ini, "graphics", "use_gpu");
+  const char *param_cursor = ini_get(ini, "graphics", "disable_cursor");
   const char *idle_ms = ini_get(ini, "graphics", "idle_ms");
   const char *param_wait = ini_get(ini, "graphics", "wait_for_device");
   const char *wait_packets = ini_get(ini, "graphics", "wait_packets");
@@ -257,7 +261,14 @@ void read_graphics_config(ini_t *ini, config_params_s *conf) {
     } else
       conf->init_use_gpu = 0;
   }
-
+  
+  if(param_cursor != NULL){
+    if (strcmpci(param_cursor, "true") == 0) {
+      conf->init_disable_cursor = 1;
+    } else
+      conf->init_disable_cursor = 0;
+  }
+  
   if (idle_ms != NULL)
     conf->idle_ms = SDL_atoi(idle_ms);
 
